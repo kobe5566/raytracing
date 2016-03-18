4743798a56cf991c64e6a3e43933866f80770717
KOBE5566 <k1234233@gmail.com>
2016-03-18 15:45:42 +0800

    LOOP UNROLL
    reduce execution time by loop unroll

--- math-toolkit_loop_unroll.h
+++ math-toolkit_loop_unroll.h
@@ -25,22 +25,17 @@ double length(const double *v)
 static inline
 void add_vector(const double *a, const double *b, double *out)
 {
- //   for (int i = 0; i < 3; i++)
    
-   		int i = 0;
-   		out[i] = a[i] + b[i];
-		out[i+1] = a[i+1] + b[i+1];
-		out[i+2] = a[i+2] + b[i+2];
+   		out[0] = a[0] + b[0];
+		out[1] = a[1] + b[1];
+		out[2] = a[2] + b[2];
 }
 
 static inline
 void subtract_vector(const double *a, const double *b, double *out)
 {
-/*    for (int i = 0; i < 3; i++)
-        out[i] = a[i] - b[i];*/
 
-   		int i = 0;
-   		out[i] = a[i] - b[i];
+   		out[0] = a[0] - b[0];
 		out[i+1] = a[i+1] - b[i+1];
 		out[i+2] = a[i+2] - b[i+2];
 }
@@ -48,26 +43,19 @@ void subtract_vector(const double *a, const double *b, double *out)
 static inline
 void multiply_vectors(const double *a, const double *b, double *out)
 {
-/*    for (int i = 0; i < 3; i++)
-        out[i] = a[i] * b[i];*/
 
-   		int i = 0;
-   		out[i] = a[i]* b[i];
-		out[i+1] = a[i+1]* b[i+1];
-		out[i+2] = a[i+2]*b[i+2];
+   		out[0] = a[0]* b[0];
+		out[1] = a[1]* b[1];
+		out[2] = a[2]*b[2];
 }
 
 static inline
 void multiply_vector(const double *a, double b, double *out)
 {
- /*   for (int i = 0; i < 3; i++)
-        out[i] = a[i] * b;
-*/
 
-   		int i = 0;
-   		out[i] = a[i] * b;
-		out[i+1] = a[i+1] * b;
-		out[i+2] = a[i+2] * b;
+   		out[0] = a[0] * b;
+		out[1] = a[1] * b;
+		out[2] = a[2] * b;
 
 
 }
@@ -85,14 +73,12 @@ double dot_product(const double *v1, const double *v2)
 {
     double dp = 0.0;
 
-    /*for (int i = 0; i < 3; i++)
-        dp += v1[i] * v2[i];*/
 	int i = 0;
-	dp += v1[i] * v2[i];
+	dp += v1[0] * v2[0];
 	
-	dp += v1[i+1] * v2[i+1];
+	dp += v1[1] * v2[1];
 
-	dp += v1[i+2] * v2[i+2];
+	dp += v1[2] * v2[2];
     return dp;
 }
 
